8cb93da7259de9b7c120a0677f65b981bd9fd14c
Carlos Lopez <cloaction@gmail.com>
2023-10-21 15:20:39 +0200

    initial

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,34 @@
-import Game from './game'
-import { World } from './world'
+import Core from './core'
+import Control from './control'
+import Player from './player'
+import Terrain from './terrain'
+import UI from './ui'
+import Audio from './audio'
 
+import './style.css'
 
-const game = new Game()
+const core = new Core()
+const camera = core.camera
+const scene = core.scene
+const renderer = core.renderer
 
-const camera = game.camera
-const scene = game.scene
-const renderer = game.renderer
+const player = new Player()
+const audio = new Audio(camera)
 
+const terrain = new Terrain(scene, camera)
+const control = new Control(scene, camera, player, terrain, audio)
 
-const world = new World(scene, camera)
-world.loadWorld()
+const ui = new UI(terrain, control)
 
-world.camera.position.y = 40
-
-
+// animation
 ;(function animate() {
   // let p1 = performance.now()
   requestAnimationFrame(animate)
 
+  control.update()
+  terrain.update()
+  ui.update()
+
   renderer.render(scene, camera)
   // console.log(performance.now()-p1)
 })()
-
